36caf0da9ba6c64ab2bede80836ecbabb2eecc5f
tchi-sama <tchisama41@gmail.com>
2023-11-15 10:19:26 -0800

    goood stuff

--- imageCreater.js
+++ imageCreater.js
@@ -7,16 +7,12 @@ const width = 27 * CM;
 const height = 40 * CM;
 const borderSize = .02 * CM;
 const spacing = .2 * CM;
-const buckets = [
-    { size: 6, img: "imgs/1.svg" },
-    { size: 6, img: "imgs/2.svg" },
-    { size: 3, img: "imgs/3.svg" },
-    { size: 6, img: "imgs/4.svg" },
-    { size: 6, img: "imgs/5.svg" },
-    { size: 9, img: "imgs/6.svg" },
-    { size: 27, img: "imgs/7.svg" },
-    { size: 2, img: "imgs/7.svg" },
-];
+const buckets = new Array(10).fill("").map((_, i) => {
+    const img = `./imgs/${Math.floor(Math.random() * 7)+1}.svg`;
+    const size = (Math.floor(Math.random() * 4)+1) * 3;
+    return ({ img, size });
+});
+
 
 const positions = fitContainer(width / CM, height/CM, buckets);
 console.log(positions)
@@ -31,7 +27,7 @@ async function main () {
                 width: width,
                 height: height,
                 channels: 4,
-                background: { r: 255, g: 255, b: 255, alpha: 1 }
+                background: { r: 255, g: 255, b: 255, alpha: .5 }
             }
         }).png().toBuffer();
         // Load the overlay image (replace 'path/to/overlay.png' with your actual file path)
@@ -39,13 +35,6 @@ async function main () {
         for (let i = 0; i < positions.length; i++) {
             const overlayImage = await sharp(positions[i].img)
                 .resize({ width: (positions[i].width * CM)-(spacing *2) , height: (positions[i].height * CM)-(spacing *2), fit: 'contain' })
-                .extend({
-                    top: borderSize,
-                    bottom: borderSize,
-                    left: borderSize,
-                    right: borderSize,
-                    background: { r: 0, g: 0, b: 0, alpha: 1 } 
-                })
                 .toBuffer();
             // Specify the position and size of the overlay
             const overlayOptions = {
