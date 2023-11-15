08cb434ab4d7c99942de58d715d06051b27139e3
tchi-sama <tchisama41@gmail.com>
2023-11-15 09:29:57 -0800

    placement workin greate

--- boxContianer.js
+++ boxContianer.js
@@ -1,65 +1,176 @@
+function fitContainer(cWidth, cHeight, buckets) {
+    // Sort buckets
+    const sortedBuckets = buckets.sort((a, b) => b.size - a.size);
+    const container = initializeContainer(cWidth, cHeight);
+    const placedElements = [];
 
-function fitContainer(cWidth,cHeight,bucket){
-    // sort buckets 
-    const sortedBuckets = bucket.sort((a,b)=>b.width-a.width)
-    const container = []
-    for (let y = 0; y < cHeight; y++) {
-        const row = []
-        for (let x = 0; x < cHeight; x++) {
-            row.push("⬛")
-        }
-        container.push(row)
-    }
+    addElement(0, 0, container, sortedBuckets, 0, placedElements);
 
-    return AddElement(0,0,container,sortedBuckets,0)
+    return placedElements;
 }
 
-function AddElement (x,y,container,buckets,i){
-    if(i==buckets.length){
-        return container
-    } 
-    const w = buckets[i].width
-    for (let loopY = 0; loopY < w; loopY++) {
-        for (let loopX = 0; loopX < w; loopX++) {
-            container[loopY+y][loopX+x]=buckets[i].name
-        }
+function initializeContainer(cWidth, cHeight) {
+    return Array.from({ length: cHeight }, () => Array(cWidth).fill("⬛"));
+}
+
+function addElement(x, y, container, buckets, i, placedElements) {
+    if (i === buckets.length) {
+        placedElements.push({ x, y, width: buckets[i - 1].size, height: buckets[i - 1].size, img: buckets[i - 1].img });
+        return;
     }
-    if(i+1<=buckets.length){
-        const wOfNext = buckets[i+1]?.width
-        if(w+x+wOfNext<=container[0].length){
-            AddElement(x+w,y,container,buckets,i+1)
-        }else{
-            let checkX = 0
-            while(container[y+w][x-checkX]=="⬛"){
-                checkX--
-            }
-            let checkY = 0
-            while(container[y+w+checkY][x+checkX-1]!=="⬛"){
-                checkY++
-            }
-            console.log(checkY)
-            if(buckets[i].w<=checkY){
-                AddElement(x+checkX,y+w,container,buckets,i+1)
+
+    const size = buckets[i].size;
+
+    for (let loopY = 0; loopY < size; loopY++) {
+        for (let loopX = 0; loopX < size; loopX++) {
+            if (container[loopY + y] && container[loopY + y][loopX + x]) {
+                container[loopY + y][loopX + x] = buckets[i].img;
             }
         }
     }
 
-    return {container }
+    placedElements.push({ x, y, width: size, height: size, img: buckets[i].img });
+
+    const nextIndex = i + 1;
+
+    if (nextIndex < buckets.length) {
+        const nextSize = buckets[nextIndex].size;
+
+        for (let newY = 0; newY <= container.length - nextSize; newY++) {
+            for (let newX = 0; newX <= container[0].length - nextSize; newX++) {
+                const canPlace = checkPlacement(container, newX, newY, nextSize);
+                if (canPlace) {
+                    addElement(newX, newY, container, buckets, nextIndex, placedElements);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function checkPlacement(container, x, y, size) {
+    for (let checkY = 0; checkY < size; checkY++) {
+        for (let checkX = 0; checkX < size; checkX++) {
+            if (container[y + checkY] && container[y + checkY][x + checkX] !== "⬛") {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 
-const containerWidth = 30;
-const containerHeight = 30;
 
-const buckets = [
-    {width:9,name:"🟩"},
-    {width:9,name:"🟥"},
-    {width:6,name:"🟪"},
-    {width:6,name:"🟨"},
-    {width:3,name:"⬜"},
-    {width:3,name:"🟫"},
-    {width:6,name:"🟧"},
-]
+exports.fitContainer = fitContainer;
 
-const result = fitContainer(containerWidth,containerHeight,buckets)
-console.log(result.container.map(a=>a.join("")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fitContainer(cWidth,cHeight,bucket){
+//     // sort buckets 
+//     const sortedBuckets = bucket.sort((a,b)=>b.width-a.width)
+//     const container = []
+//     for (let y = 0; y < cHeight; y++) {
+//         const row = []
+//         for (let x = 0; x < cHeight; x++) {
+//             row.push("⬛")
+//         }
+//         container.push(row)
+//     }
+
+//     return AddElement(0,0,container,sortedBuckets,0)
+// }
+
+// function AddElement (x,y,container,buckets,i){
+//     if(i==buckets.length){
+//         return container
+//     } 
+//     const w = buckets[i].width
+//     for (let loopY = 0; loopY < w; loopY++) {
+//         for (let loopX = 0; loopX < w; loopX++) {
+//             container[loopY+y][loopX+x]=buckets[i].name
+//         }
+//     }
+//     if(i+1<=buckets.length){
+//         const wOfNext = buckets[i+1]?.width
+//         if(w+x+wOfNext<=container[0].length){
+//             AddElement(x+w,y,container,buckets,i+1)
+//         }else{
+//             let checkX = 0
+//             while(container[y+w][x-checkX]=="⬛"){
+//                 checkX--
+//             }
+//             let checkY = 0
+//             while(container[y+w+checkY][x+checkX-1]!=="⬛"){
+//                 checkY++
+//             }
+//             console.log(checkY)
+//             if(buckets[i].w<=checkY){
+//                 AddElement(x+checkX,y+w,container,buckets,i+1)
+//             }
+//         }
+//     }
+
+//     return {container }
+// }
+
+
+// const containerWidth = 30;
+// const containerHeight = 30;
+
+// const buckets = [
+//     {width:9,name:"🟩"},
+//     {width:9,name:"🟥"},
+//     {width:6,name:"🟪"},
+//     {width:6,name:"🟨"},
+//     {width:3,name:"⬜"},
+//     {width:3,name:"🟫"},
+//     {width:6,name:"🟧"},
+// ]
+
+// const result = fitContainer(containerWidth,containerHeight,buckets)
+// console.log(result.container.map(a=>a.join("")))
--- imageCreater.js
+++ imageCreater.js
@@ -1,16 +1,28 @@
 const sharp = require('sharp');
+const { fitContainer } = require('./boxContianer');
 
 
+const CM = 50;
+const width = 27 * CM;
+const height = 40 * CM;
+const borderSize = .02 * CM;
+const spacing = .2 * CM;
+const buckets = [
+    { size: 6, img: "imgs/1.svg" },
+    { size: 6, img: "imgs/2.svg" },
+    { size: 3, img: "imgs/3.svg" },
+    { size: 6, img: "imgs/4.svg" },
+    { size: 6, img: "imgs/5.svg" },
+    { size: 9, img: "imgs/6.svg" },
+    { size: 27, img: "imgs/7.svg" },
+    { size: 2, img: "imgs/7.svg" },
+];
+
+const positions = fitContainer(width / CM, height/CM, buckets);
+console.log(positions)
 
 
-
-
-
-
-(async function () {
-    const CM = 300;
-    const width = 27 * CM;
-    const height = 40 * CM;
+async function main () {
 
     try {
         // Create the base image with a red semi-transparent background
@@ -19,34 +31,33 @@ const sharp = require('sharp');
                 width: width,
                 height: height,
                 channels: 4,
-                background: { r: 0, g: 0, b: 0, alpha: 1 }
+                background: { r: 255, g: 255, b: 255, alpha: 1 }
             }
         }).png().toBuffer();
-        const borderSize = .02 * CM;
         // Load the overlay image (replace 'path/to/overlay.png' with your actual file path)
         const stickers = []
-        for (let i = 0; i < 3; i++) {
-            const overlayImage = await sharp('imgs/image1.png')
-                .resize({ width: 9 * CM, height: 9 * CM, fit: 'contain' })
+        for (let i = 0; i < positions.length; i++) {
+            const overlayImage = await sharp(positions[i].img)
+                .resize({ width: (positions[i].width * CM)-(spacing *2) , height: (positions[i].height * CM)-(spacing *2), fit: 'contain' })
                 .extend({
                     top: borderSize,
                     bottom: borderSize,
                     left: borderSize,
                     right: borderSize,
-                    background: { r: 255, g: 255, b: 255, alpha: 1 } 
+                    background: { r: 0, g: 0, b: 0, alpha: 1 } 
                 })
                 .toBuffer();
             // Specify the position and size of the overlay
             const overlayOptions = {
-                top: 0,    // Specify the top position in pixels
-                left: i * (9 * CM  ) ,  // Specify the left position in pixels
+                top: positions[i].y * CM + spacing,    // Specify the top position in pixels
+                left: positions[i].x * CM + spacing,  // Specify the left position in pixels
             };
             stickers.push({input:overlayImage, ...overlayOptions})
         }
         // Composite the overlay onto the base image
         const compositeImage = await sharp(baseImage)
             .composite(stickers)
-            .toFile(`output/finalImage-${Date.now()}.png`)
+            .toFile(`output/finalImage-.png`)
             .then(() => {
                 console.log('Image created successfully.');
             })
@@ -56,4 +67,9 @@ const sharp = require('sharp');
     } catch (err) {
         console.error(err);
     }
-})();
+}
+
+
+
+main()
+
